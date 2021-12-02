5f7b3d57913c1df143407afec097799cd591b763
ben-meneses-sosa <3712124+benhas@users.noreply.github.com>
2021-12-03 09:54:35 +1100

    Part 1 done; part 2 in progress; author email reset

---
+++ SonarSweep/SonarSweep.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SonarSweep
+{
+    public class SonarSweep
+    {
+        private static bool DepthIncrease(int? previousMeasure, int thisMeasure)
+        {
+            return thisMeasure > previousMeasure;
+        }
+
+        public static int CalculateDepthIncreases(int[] depths)
+        {
+            var increases = 0;
+            var lastDepth = 0;
+            
+            for (var i = 0; i < depths.Length; i++)
+            {
+                if (i > 0)
+                {
+                    var thisDepth = depths[i];
+                    increases += (DepthIncrease(lastDepth, thisDepth) ? 1 : 0);
+                }
+
+                lastDepth = depths[i];
+            }
+
+            return increases;
+        }
+
+        public static int[] GetRollingSumArray(int[] depths)
+        {
+            /* WIP */
+            var rollingSumsArray = new List<int>();
+            
+            if (depths.Length <= 3)
+            {
+                var rollingSumDepth = depths.Sum();
+                rollingSumsArray.Add(rollingSumDepth);
+            }
+            var firstDepth = 0;
+            var secondDepth = 0;
+            var thirdDepth = 0;
+            for (var i = 0; i < depths.Length; i++)
+            {
+                var depthsSum = 0;
+                if (i < 2)
+                {
+                    firstDepth = depths[i];
+                }
+            }
+            return rollingSumsArray.ToArray();
+        }
+    }
+}
---
+++ SonarSweepTests/SonarSweepTests.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualBasic;
+using Xunit;
+
+namespace SonarSweepTests
+{
+    public class SonarSweepTests
+    {
+        [Fact]
+        public void CalculateDepthIncreases_DepthsArray_ReturnIncreases()
+        {
+            var depths = new[] {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+            const int expectedIncreases = 7;
+            var foundIncreases = SonarSweep.SonarSweep.CalculateDepthIncreases(depths);
+            Assert.Equal(expectedIncreases, foundIncreases);
+        }
+
+        [Fact]
+        public void CalculateDepthRollingSums_DepthsArray_ReturnRollingSumsArray()
+        {
+            /*var depths = new[] {199, 200, 208, 210, 200, 207, 240, 269, 260, 263};
+            var expectedDepths = new[] { 607, 618,618,617,647,716,769,792};*/
+            var depths = new[] {199, 200, 208};
+            var expectedDepths = new[] { 607};
+            var foundDepths = SonarSweep.SonarSweep.GetRollingSumArray(depths);
+            Assert.Equal(expectedDepths, foundDepths);
+        }
+    }
+}
